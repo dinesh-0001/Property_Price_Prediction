d2cd8e7b5002356a29bd44f1680e795e31b02755
Dinesh Kumar <dsaharan800@gmail.com>
2023-02-06 22:44:05 +0530

    Done for the day (2/6/23)

--- Server/util.py
+++ Server/util.py
@@ -1,8 +1,24 @@
 import pickle
 import json
+import numpy as np
 __locations =None
 __data_columns=None
 __model=None
+
+
+def get_estimated_price(location, Total_Area, bhk, Baths):
+    try:    
+        loc_index=__data_columns.index(location.lower())
+    except:
+        loc_index=-1
+    
+    x=np.zeros(len(__data_columns))
+    x[0]=Total_Area
+    x[1]=bhk
+    x[2]=Baths
+    if loc_index>=0:
+        x[loc_index]=1
+    return round(__model.predict([x])[0],2)
 
 def get_location_names():
     return __locations
@@ -12,10 +28,11 @@ def load_saved_artifacts():
     global __locations
     global __data_columns
 
-    with open("C:/Users/Dinesh/OneDrive/Documents/Projects_Portfolio/Property_Price_Prediction/Server/artifacts/columns.json",'r') as f:
+    with open("/artifacts/columns.json",'r') as f:
         __data_columns = json.load(f)['data_columns']
         __locations=__data_columns[3:]
     
+    global __model
     with open("C:/Users/Dinesh/OneDrive/Documents/Projects_Portfolio/Property_Price_Prediction/Server/artifacts/property_price_prediction.pickle",'rb') as f:
         __model = pickle.load(f)
     print("loading saved artifacts...done")
@@ -24,3 +41,4 @@ def load_saved_artifacts():
 if __name__=='__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('Electronic City Bangalore', 1800, 4, 4))
